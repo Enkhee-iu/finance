6946765e8c0893bf37bd2a5e1572d64691ce4ae9
enkhee-iu <enkheetuyatsetseg@gmail.com>
2026-07-30 00:53:05 +0800

    js

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 let uiController = (function (){
-  
+
+   let Domstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+   };
+
+  return{
+    getInput: function () {
+      return {
+        type: document.querySelector(Domstrings.inputType).value,
+        description: document.querySelector(Domstrings.inputDescription).value,
+        value: document.querySelector(Domstrings.inputValue).value
+      };
+    },
+
+    getDomstrings: function () {
+      return Domstrings;
+    }
+    };
 })();
 
 let financeController = (function (){
@@ -9,21 +29,41 @@ let financeController = (function (){
 
 let appController = (function (uicontroller, financecontroller){
 
+  let DOM = uicontroller.getDomstrings();s
+
    let ctrlAddItem = function () {
     console.log("Delegtsend ugugdul awah heseg");
    };
 
- document.querySelector(".add__btn").addEventListener("click", function(){
+ document.querySelector(Domstrings.inputBtn).addEventListener("click", function(){
 
  });
 
-  document.addEventListener("keypress", function(event){
+
+
+  let setupEventListeners = function () {
+    let DOM = uicontroller.getDomstrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", function () {
+      ctrlAddItem();
+    });
+
+      document.addEventListener("keypress", function(event){
     if (event.keyCode === 13) {
       ctrlAddItem();
     }
   });
+  }
+
+  return{
+    init: function () {
+      console.log("Application started");
+      setupEventListeners();
+    }
+  };
 
 })(uiController, financeController);
+
+appController.init();
 
 
 
